83f4fed8345ad61a4876061275e4f968911c499e
LeDindonDeLaForce <96035629+LeDindonDeLaForce@users.noreply.github.com>
2022-06-01 08:41:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,18 +11,19 @@ import custom_commands
 from twitchio import Channel, Client, User
 from twitchio.ext import commands, pubsub, routines
 
-from utils import auto_so, check_for_bot, random_bot_reply, random_reply
-from db import init_channels, add_channel, leave_channel
+from utils import check_for_bot, random_bot_reply, random_reply, auto_so
 
+tw_channels=["type", "here", "each", "channel", "which", "uses", "this", "bot"]
 
-class LeixBot(commands.Bot):
+class ledindobot(commands.Bot):
 
     def __init__(self):
         super().__init__(
             token=os.environ['ACCESS_TOKEN'],
             prefix=os.environ['BOT_PREFIX'],
-            client_id=os.environ['CLIENT_ID'],
-            initial_channels=init_channels(),
+#            client_id=os.environ['CLIENT_ID'],
+#            initial_channels=init_channels(),
+            initial_channels=tw_channels,
             case_insensitive=True
         )
         self.pubsub_client = None
@@ -31,14 +32,15 @@ class LeixBot(commands.Bot):
             p.stem for p in Path(".").glob("./cogs/*.py")
         ]
         self.vip_so = {
-            x: {} for x in init_channels()
+            x: {} for x in tw_channels
         }
-        self.bot_to_reply = ['wizebot', 'streamelements', 'nightbot', 'moobot']
+        self.bot_to_reply = ['wizebot', 'streamelements', 'nightbot', 'moobot', 'bothagonas', 'nitr0genbot']
         self.giveaway = set()
-        self.routines = {}
 
     def setup(self):
         random.seed()
+
+        logging.info(f'{self._http.client_id}')
         logging.info("Chargement des cogs...")
 
         for cog in self._cogs_names:
@@ -59,10 +61,10 @@ class LeixBot(commands.Bot):
         uu: User = u[0]
 
         topics = [
-            pubsub.channel_points(self.pubsub_client._http.token)[uu.id]
+            #pubsub.channel_points(self.pubsub_client._http.token)[uu.id]
         ]
-        await self.pubsub_client.pubsub.subscribe_topics(topics)
-        await self.pubsub_client.connect()
+        #await self.pubsub_client.pubsub.subscribe_topics(topics)
+        #await self.pubsub_client.connect()
         self.channel = self.get_channel(os.environ['CHANNEL'])
 
         # Starting timers
@@ -95,13 +97,13 @@ class LeixBot(commands.Bot):
         #     message.author.channel.send(
         #         f'/ban {message.author.name} Vilain Bot')
 
-        if message.content[0] == os.environ['BOT_PREFIX']:
+        if message.content[0] == os.environ['BOT_PREFIX']: # si une custom command est jouée
             reply = custom_commands.find_command(message)
-            if reply is not None:
-                await message.author.channel.send(reply)
+            if reply is not None: #await message.author.channel.send(reply) [custom command sans tag de celui qui l'a jouée]
+                await message.author.channel.send(f"@{message.author.name} {reply}") # [custom command avec tag de celui qui l'a jouée]
                 return
 
-        if "@leixbot" in message.content.lower():
+        if "@ledindobot" in message.content.lower(): #AUTO SO et réponses aléatoires du bot
             await random_reply(self, message)
         elif message.author.name.lower() in self.bot_to_reply:
             await random_bot_reply(message)
@@ -113,23 +115,24 @@ class LeixBot(commands.Bot):
         await self.handle_commands(message)
 
     async def event_raw_usernotice(self, channel, tags):
+        logging.debug(tags)
         if tags["msg-id"] == "sub":
-            await channel.send(f"/me PogChamp {tags['display-name']} rejoint la légion! Merci pour le sub PogChamp")
+            await channel.send(f"/me PogChamp {tags['display-name']} rejoint l'ordre de la plume d'Or! Merci pour le sub PogChamp")
         elif tags["msg-id"] == "resub":
             await channel.send(
-                f"/me PogChamp Le resub de {tags['display-name']}!! Merci de fièrement soutenir la chaine depuis {tags['msg-param-cumulative-months']} mois <3"
+                f"/me PogChamp Le resub de {tags['display-name']}!! Merci pour ton {tags['msg-param-cumulative-months']}ème mois <3"
             )
         elif tags['msg-id'] == 'subgift':
             await channel.send(
-                f'/me {tags["display-name"]} est vraiment trop sympa, il régale {tags["msg-param-recipient-display-name"]} avec un sub!'
+                f'/me {tags["display-name"]} est vraiment trop sympa, il régale {tags["msg-param-recipient-display-name"]} avec un subgift !'
             )
         elif tags['msg-id'] == 'anonsubgift':
             await channel.send(
-                f'/me Un donateur anonyme est vraiment trop sympa, il régale {tags["msg-param-recipient-display-name"]} avec un sub!'
+                f'/me Un donateur anonyme est vraiment trop sympa, il régale {tags["msg-param-recipient-display-name"]} avec un subgift !'
             )
         elif tags["msg-id"] == "raid":
             await channel.send(
-                f"/me Il faut se défendre SwiftRage ! Nous sommes raid par {tags['msg-param-displayName']} et ses {tags['msg-param-viewerCount']} margoulins!"
+                f"/me LEZ GO OEEE y'a un raid de {tags['msg-param-displayName']} avec {tags['msg-param-viewerCount']} margoulins qu'arrive !"
             )
 
     async def event_command_error(self, ctx: commands.Context, error: Exception):
@@ -154,16 +157,18 @@ class LeixBot(commands.Bot):
             self.giveaway.add(event.user.name)
 
     ## ROUTINES ##
-    @routines.routine(minutes=30.0, wait_first=False)
+    @routines.routine(minutes=40.0, wait_first=False)
     async def links(self):
-        await self.channel.send("Mon YouTube: https://youtube.com/leix34")
-        await asyncio.sleep(60 * 30)
-        await self.channel.send("Guide Apex Legends: https://leochely.github.io/apexLegendsGuide/")
-        await asyncio.sleep(60 * 30)
-        await self.channel.send("Le discord: https://discord.com/invite/jzU7xWstS9")
-        await asyncio.sleep(60 * 30)
-        await self.channel.send("Un giveaway de jeux est en cours! Pour 5k slayer points, vous pouvez avoir une chance de remporter un des jeux du giveaway (liste complete dans la recompense de chaine)")
-        await asyncio.sleep(60 * 30)
+        await self.channel.send("Moi robot MrDestructoid Kappa")
+        await asyncio.sleep(60 * 40)
+        await self.channel.send("Il se passe un truc incroyable ? Le dindon se fait dindonned ? Lâche un clip, fais toi plaiz ! TTours")
+        await asyncio.sleep(60 * 40)
+        await self.channel.send("Si le contenu de la chaîne te plaît, hésite pas à follow pour ne pas manquer la suite ledind1Hey")
+        await asyncio.sleep(60 * 40)
+        await self.channel.send("Mon serveur discord: https://discord.gg/7QyK5U3BKD ImTyping")
+        await asyncio.sleep(60 * 40)
+
+
 
     @commands.command(name="routine_add")
     async def routine_add(self, ctx: commands.Context, name, seconds, minutes, hours, *text):
@@ -201,33 +206,38 @@ class LeixBot(commands.Bot):
             return
 
         # Stops routine
+        logging.info(self.routines)
         self.routines[ctx.author.channel.name + '_' + name].cancel()
+        logging.info(self.routines)
 
-        # Removes routine from db
         custom_commands.remove_routine(ctx.author.channel.name, name)
         await ctx.send('Routine stoppée avec succès MrDestructoid')
+
+
+
 
     ## GENERAL FUNCTIONS ##
     @commands.command(name="git")
     async def git(self, ctx: commands.Context):
         await ctx.send(
-            f'Here is my source code https://github.com/leochely/leixbot/ MrDestructoid'
+            f'Le code source de mon papa LeixBot qui a créé le bot que je suis : https://github.com/leochely/LeixBot MrDestructoid'
         )
 
     @commands.command(name="list")
     async def list(self, ctx: commands.Context):
-        list = ""
+        list = []
         for command in self.commands:
-            list += command + ", "
+            list.append(command)
 
-        # Remove last comma and space
-        list = list[:-2]
-        await ctx.send(f'La liste des commandes de LeixBot: {list}')
+        list.sort()
+        list = str(list)[1:-1] #remove list square brackets
+
+        await ctx.send(f'La liste des commandes de ledindobot (hors custom) : {list}')
+
 
     @commands.command(name="join")
     async def join(self, ctx: commands.Context, channel):
         if ctx.author.name == os.environ['CHANNEL'] or ctx.author.name == channel:
-            channel = channel.lower()
             await ctx.send(f'Joining channel {channel}')
 
             await self.join_channels({channel})
@@ -237,7 +247,6 @@ class LeixBot(commands.Bot):
     @commands.command(name="leave")
     async def leave(self, ctx: commands.Context, channel):
         if ctx.author.name == os.environ['CHANNEL'] or ctx.author.name == channel:
-            channel = channel.lower()
             await ctx.send(f'Leaving channel {channel}')
 
             logging.info(f"PART #{channel}\r\n")
@@ -269,7 +278,7 @@ if __name__ == "__main__":
             logging.StreamHandler()
         ]
     )
-
+"""
     client = Client(
         token=os.environ['CHANNEL_ACCESS_TOKEN'],
         initial_channels=os.environ['CHANNEL'],
@@ -280,8 +289,7 @@ if __name__ == "__main__":
 
     @client.event()
     async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
-        await bot.event_pubsub_channel_points(event)
-
-    bot = LeixBot()
-    bot.pubsub_client = client
-    bot.run()
+    await bot.event_pubsub_channel_points(event)
+"""
+bot = ledindobot()
+bot.run()
