e54a8a240c0da16d98385931a98ae192c99f6728
LeDindonDeLaForce <96035629+LeDindonDeLaForce@users.noreply.github.com>
2022-06-09 18:12:52 +0200

    Add files via upload

---
+++ sqlcleaner.py
@@ -0,0 +1,11 @@
+def cleandata(data): #this script is here to clean data from additionnal character, to make it usable as a list
+    newdata = []
+    for sample in data:
+        tempdata = str(sample)
+        tempdata = tempdata.replace('(','')
+        tempdata = tempdata.replace(')','')
+        tempdata = tempdata.replace(',','')
+        tempdata = tempdata.replace('\'','')
+        newdata.append(tempdata)
+
+    return newdata
